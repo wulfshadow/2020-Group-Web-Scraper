17ef2d802dc4d34242a955846be8a0fad89c60c9
Sam Laos <43764945+wulfshadow@users.noreply.github.com>
2020-02-12 13:11:56 -0800

    Update magicHappens.py

--- magicHappens.py
+++ magicHappens.py
@@ -1,12 +1,29 @@
 # Will be edited later to find specfic information
-def get_text():
-    page_response = requests.get(page_link, timeout=5)
-    page_content = BeautifulSoup(page_response.content, "html.parser")
-    text = str(page_content)
-    raw_text = html2text.html2text(text)
-    return(raw_text)
+# def get_text():
+#     page_response = requests.get(page_link, timeout = 5)
+#     page_content = BeautifulSoup(page_response.content, "html.parser")
+#     text = str(page_content)
+#     raw_text = html2text.html2text(text)
+#     return(raw_text)
 
-current_text = get_text()
+# current_text = get_text()
 
 # For my use later:
 # <span class="YVvGBb asQXV">vizceral posted a new assignment: test</span>
+
+from HTMLParser import HTMLParser
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print "Encountered a start tag:", tag
+
+    def handle_endtag(self, tag):
+        print "Encountered an end tag :", tag
+
+    def handle_data(self, data):
+        print "Encountered some data  :", data
+
+# instantiate the parser and fed it some HTML
+parser = MyHTMLParser()
+parser.feed('<span class="YVvGBb asQXV">vizceral posted a new assignment: test</span>'
