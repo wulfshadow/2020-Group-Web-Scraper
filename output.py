2b6c740dde372966367b35655c4b2e9ad58d08a6
Sam Laos <43764945+wulfshadow@users.noreply.github.com>
2020-03-01 18:59:24 -0800

    Update output.py

--- output.py
+++ output.py
@@ -1,8 +1,7 @@
-client = Client(account_sid, auth_token)
-
-message = client.messages.create(
-    to=output_number, 
-    from_=twilio_number,
-    body=output_message)
-
-print(message.sid)
+        #OUTPUT
+        message = client.messages.create(
+            to = output_number, 
+            from_= twilio_number,
+            body = 'New tag detected on ' + str(page_link))
+        current_count = page_count
+    time.sleep(time_wait)
