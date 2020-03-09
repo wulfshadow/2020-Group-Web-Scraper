06fe92b9b6ce3eaaab73ca82e5831c92e8d8ee64
Sam Laos <43764945+wulfshadow@users.noreply.github.com>
2020-03-09 12:52:06 -0700

    Update input.py

--- input.py
+++ input.py
@@ -10,7 +10,6 @@ from varscraper import *
 client = Client(account_sid, auth_token)
 page_link = raw_input("Enter a URL: ")
 page_link = page_link.strip()
-#page_link = 'https://wulfshadow.github.io/Holiday-Contest/'
 getWaitTime = raw_input("Number of seconds between searches:")
 time_wait = int(getWaitTime)
 
